c2cf7db1bdb444d12575fb67fd03deb194a6246a
Can Berk Ocalir <can.ocalir@gmail.com>
2022-12-20 19:11:11 +0300

    fixed exec with shelljs

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 #!/usr/bin/env node
-const {exec} = require('child_process')
-const createdjango = exec('bash createdjango', 
-(error, stdout, stderr) => {
-    console.log(stdout);
-    console.log(stderr);
-    if(error){
-        console.log(`exec error: ${error}`)
-    }
-})
+const shell = require('shelljs')
+shell.exec('./createdjango')
